5a0ba74098d5b70767808741477be34c460fd32f
Pablo Murga <pmurgapita@gmail.com>
2026-02-10 13:11:34 +0100

    The game is finished
    I made two functions one to see who wins, another to keep score in 5 games

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 console.log("Hello World");
 //Input one of the three choices (rock, paper or scissors)
-let choice = prompt("Rock, Paper, or Scissors?");
+function choice(){
+    let choice = prompt("Rock, Paper, or Scissors?");
+    return choice
+}
 function getHumanChoice(choice) {
     let humChoice = "";
     if (choice == "Rock" || choice == "Paper" || choice == "Scissors") {
@@ -8,9 +11,9 @@ function getHumanChoice(choice) {
     } else {
         humChoice = "";
     }
-    return "Your choice is: " + humChoice;
+    return humChoice;
 }
-console.log(getHumanChoice(choice));
+
 
 //Make the computer choose one too randomly
 function getComputerChoice(){
@@ -23,23 +26,43 @@ function getComputerChoice(){
     } else {
         compChoice = "Paper";
     }
-    return "Computer´s choice is: " + compChoice;
-}
-console.log(getComputerChoice());
-
-//Tell the program how the game works rock wins to scissors, sissors wins to paper, and paper wins to rock
-function playRound (humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice == computerChoice) {
-        console.log("That´s a draw! " + humanChoice + " is equal to " + compChoice);
-    }
-    switch (humanChoice) {
-        case "rock":
-            if (compChoice == "Paper")
-    }
+    return compChoice;
 }
 
-//Tell the program how to keep score
-let humanScore = 0;
-let computerScore = 0;
+
+
+
+
+function playGame () {
+    //Tell the program how to keep score
+    let humanScore = 0;
+    let computerScore = 0;
+
+    //Tell the program how the game works rock wins to scissors, sissors wins to paper, and paper wins to rock
+    function playRound (humanChoice, computerChoice) {
+        console.log("Your choice is: " + humanChoice);
+        console.log("Computer´s choice is: " + computerChoice);
+        lowerCaseHumanChoice = humanChoice.toLowerCase();
+        if (lowerCaseHumanChoice == computerChoice.toLowerCase()) {
+            console.log("That´s a draw! " + humanChoice + " is equal to " + computerChoice);
+        } else if (lowerCaseHumanChoice === "rock" && computerChoice === "Paper" || lowerCaseHumanChoice === "paper" && computerChoice === "Scissors" || lowerCaseHumanChoice === "scissors" && computerChoice === "Rock") {
+            console.log("You lost! " + computerChoice + " beats " + humanChoice)
+            computerScore++;
+        } else {
+            console.log("You won! " + humanChoice + " beats " + computerChoice)
+            humanScore++;
+        }
+    }
+    for (let i = 1; i <= 5; i++) {
+        playRound(getHumanChoice(choice()), getComputerChoice());
+    }
+    if (humanScore < computerScore) {
+        console.log ("The Computer won: " + computerScore + "-" + humanScore)
+    } else {
+        console.log ("The Score was: " + humanScore + "-" + computerScore)
+    }
+}
+playGame();
+
+
 

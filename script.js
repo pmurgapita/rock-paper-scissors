ea30759107f559bdc46142fb93592c9b19752713
Pablo Murga <pmurgapita@gmail.com>
2026-02-11 23:18:12 +0100

    Added the start over function and disabled buttons
    Adding a button, and the function to begin again, also disabled the buttons to prevent errors

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-
 const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
@@ -30,7 +29,10 @@ score.textContent = humanScore + " - " + computerScore;
 divScore.appendChild(score);
 
 const result = document.createElement("p");
+result.textContent = "Rock, Paper or Scissors?"
 divResult.appendChild(result);
+
+const endGame = document.createElement("p");
 
 //Tell the program how the game works rock wins to scissors, sissors wins to paper, and paper wins to rock
 function playRound (humanChoice, computerChoice) {
@@ -72,22 +74,37 @@ scissorsButton.addEventListener("click", () => {
     score.textContent = humanScore + " - " + computerScore;
 } );
 
-const button = document.querySelectorAll("button");
-button.forEach(button => button.addEventListener('click', () =>{
+const button = document.querySelectorAll("button.div");
+button.forEach(button => button.addEventListener('click', () => {
     if (humanScore === 5 || computerScore ===5) {
         divScore.removeChild(score);
         divResult.removeChild(result);
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
         if (humanScore < computerScore) {
-            const endGame = document.createElement("p");
             endGame.textContent = "The Computer won! Total Score: " + computerScore + "-" + humanScore;
             divScore.appendChild(endGame);
         } else {
-            const endGame = document.createElement("p");
             endGame.textContent = "You won! Total Score: " + humanScore + "-" + computerScore;
             divScore.appendChild(endGame);
         }
     }
 }));
+
+const startOver = document.querySelector(".startOver");
+startOver.addEventListener("click", () => {
+    if (divScore.contains(endGame)) {divScore.removeChild(endGame)} 
+    result.textContent = "Rock, Paper or Scissors?"
+    score.textContent = "0 - 0"
+    divScore.appendChild(score);
+    divResult.appendChild(result);
+    humanScore = 0;
+    computerScore = 0;
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+});
 
 
 
